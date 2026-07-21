e828d4e794e935822dd6a0db4d8a8a5700f72f6c
Leon <LAnneken09@gmail.com>
2026-07-21 19:54:45 +0200

    Ordering the matches now

--- main/functions/match/get-all.ts
+++ main/functions/match/get-all.ts
@@ -1,7 +1,10 @@
+import { desc } from "drizzle-orm";
 import { db } from "../../../db";
+import { matches } from "../../../db/schema";
 
 export async function match_getAll() {
   return db.query.matches.findMany({
+    orderBy: desc(matches.startedAt),
     with: {
       players: true,
       kills: true,
