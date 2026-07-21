2cc3ebf47dcf225563999b85809428242cb7840b
Leon <LAnneken09@gmail.com>
2026-07-21 19:54:06 +0200

    Removed the returning part

--- main/functions/db/clear.ts
+++ main/functions/db/clear.ts
@@ -2,5 +2,5 @@ import { db } from "../../../db";
 import { matches } from "../../../db/schema";
 
 export async function db_clear() {
-  return db.delete(matches).returning().all();
+  return db.delete(matches);
 }
